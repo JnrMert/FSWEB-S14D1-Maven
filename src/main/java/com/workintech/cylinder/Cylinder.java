768d1520c1d92f72a11e74168d1ac433866d7953
Mert Yazıcı | Newer! <yzc.mert@icloud.com>
2024-09-23 19:56:16 +0300

    symbol:   method assertTrue(boolean) 61 - 81

---
+++ src/main/java/com/workintech/cylinder/Cylinder.java
@@ -0,0 +1,23 @@
+package com.workintech.cylinder;
+
+public class Cylinder extends Circle {
+    private double height;
+
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume() {
+        return height * getArea();
+    }
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        if(height<0){
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+
+}
